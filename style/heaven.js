edb6be7adc0f9a1a44adf2485dc133151ad47b65
grmdhn20 <72301067+grmdhn20@users.noreply.github.com>
2020-12-03 22:11:20 +0700

    3 Dec

--- style/heaven.js
+++ style/heaven.js
@@ -1,7 +1,17 @@
-function myFunction(){
+function form(){
     const email = document.forms["form"]["email"].value;
     const kritik = document.forms["form"]["kritik"].value;
     const tableOutput = document.getElementById('show')
     tableOutput.rows[1].cells[0].innerHTML = email
     tableOutput.rows[1].cells[1].innerHTML = kritik
 }
+
+function button() {
+    var txt;
+    if (confirm("Terima kasih atas dukungan anda")) {
+      txt = "Selesai";
+    } else {
+      txt = "Cancel!";
+    }
+    document.getElementById("demo").innerHTML = txt;
+  }
