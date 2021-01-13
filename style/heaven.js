76c13cfc20ebf02c9fc7c9f9c0aac5ba61bef7e4
grmdhn20 <72301067+grmdhn20@users.noreply.github.com>
2021-01-13 19:55:39 +0700

    Update heaven.js

--- style/heaven.js
+++ style/heaven.js
@@ -6,6 +6,7 @@ function form(){
     tableOutput.rows[1].cells[1].innerHTML = kritik
 }
 
+<<<<<<< HEAD
 function button() {
     var txt;
     if (confirm("Terima kasih atas dukungan anda")) {
@@ -15,3 +16,45 @@ function button() {
     }
     document.getElementById("demo").innerHTML = txt;
   }
+=======
+var navbar = document
+    .querySelectorAll(
+        'ul.navbar-responsive li.navbar-children a'
+    );
+console.log('links', navbar);
+navbar.forEach(function(link) {
+    console.log('each link', link);
+});
+
+var submenu = navbar.length;
+for (var i=0; i<submenu; i++) {
+    var link = navbar[i];
+    console.log('each submenu', link)
+}
+navbar.forEach(function (link){
+    link.addEventListener('click', function (ev){
+        console.log('link clicked', link)
+    var parent = link.parentElement;
+    if (parent.classList.contains('active')){
+        parent.classList.remove('active');}
+    else {
+        parent.classList.add('active')
+        }
+    })
+});
+
+// fungsi pop upimage
+// Get the modal
+var modal = document.getElementById("thepopup");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("popup-img");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+function showImg(ele){
+    modal.style.display = "block";
+    modalImg.src = ele.src;
+    captionText.innerHTML = ele.alt;
+}
+var close = document.getElementsByClassName("close")[0];
+>>>>>>> 71afccb04dd78468a7d765a475c9646be56ccfff
